03e540a1238d0156bbfe42670383100ca0c09f0b
Sabuj <sabujlovescoding@gmail.com>
2019-10-28 22:21:22 +0530

    Add Post method added

---
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent  {
+
+ onAddPost() {
+   console.log('Post is added!!');
+ }
+
+}
