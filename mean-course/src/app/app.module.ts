67fd3e7c12eb77c694a67d51c0e343ed927cd550
Sabuj <sabujlovescoding@gmail.com>
2019-10-29 17:17:55 +0530

    Added post-list componenet

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
