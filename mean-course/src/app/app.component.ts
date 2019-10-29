23d4c676543e6366aa417af279003f59acab2a54
Sabuj <sabujlovescoding@gmail.com>
2019-10-29 18:25:05 +0530

    Step 2 - Receive the post in app-component

--- mean-course/src/app/app.component.ts
+++ mean-course/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mean-course';
+
+  posts = []; // global posts array
+
+  onPostAdded(post) {
+    this.posts.push(post);
+  }
+
 }
