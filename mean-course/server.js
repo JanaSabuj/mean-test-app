f409399521d580cffb18d89c82bfd08f4cfb89ff
Sabuj <sabujlovescoding@gmail.com>
2019-10-30 10:01:00 +0530

    Initial server up and running

--- mean-course/server.js
+++ mean-course/server.js
@@ -1,1 +1,9 @@
 console.log('Hello world');
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('This is my first response');// end the response stream
+});
+
+server.listen( process.env.PORT || 3000);
